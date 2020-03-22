e5d652086cfad2a20049748ac75e685914e77155
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-03-23 02:02:00 +0800

    [DEV] add husky and lint-staged
    [EDITOR]: add Editor route

---
+++ client/src/routes/Editor/Editor.tsx
@@ -0,0 +1,34 @@
+import React, { useMemo, useState } from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
+
+interface Props {}
+
+const Editor: React.FC<Props> = () => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }]
+    }
+  ]);
+
+  return (
+    <div style={{ paddingLeft: "3rem", paddingRight: "3rem" }}>
+      <Slate
+        editor={editor}
+        value={value}
+        onChange={(value: any) => setValue(value)}
+      >
+        <Editable
+          style={{
+            backgroundColor: "#fafafa",
+            padding: "2rem"
+          }}
+        />
+      </Slate>
+    </div>
+  );
+};
+
+export default Editor;
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Todos from "./Todos";
+import Editor from "./Editor";
 
 interface Props {}
 
@@ -8,6 +9,7 @@ const Index: React.FC<Props> = () => {
   return (
     <Router>
       <Route path="/todos" component={Todos} />
+      <Route path="/editor" component={Editor} />
     </Router>
   );
 };
