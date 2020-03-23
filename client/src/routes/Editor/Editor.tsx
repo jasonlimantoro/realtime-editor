1b4ca76ba75dfe56704746f01907e06b536c6bc2
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-03-23 11:48:42 +0800

    [REFACTOR]: use tailwind class instead of style object

--- client/src/routes/Editor/Editor.tsx
+++ client/src/routes/Editor/Editor.tsx
@@ -9,23 +9,18 @@ const Editor: React.FC<Props> = () => {
   const [value, setValue] = useState<Node[]>([
     {
       type: "paragraph",
-      children: [{ text: "A line of text in a paragraph." }]
-    }
+      children: [{ text: "A line of text in a paragraph." }],
+    },
   ]);
 
   return (
-    <div style={{ paddingLeft: "3rem", paddingRight: "3rem" }}>
+    <div className="p-8">
       <Slate
         editor={editor}
         value={value}
         onChange={(value: Node[]) => setValue(value)}
       >
-        <Editable
-          style={{
-            backgroundColor: "#fafafa",
-            padding: "2rem"
-          }}
-        />
+        <Editable className="p-8 bg-gray-200" />
       </Slate>
     </div>
   );
