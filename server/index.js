e879dd47bb3d8dfa49564475a3f0f05bb8886068
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-03-29 23:42:14 +0800

    [SERVER]: add mongodb

--- server/index.js
+++ server/index.js
@@ -3,6 +3,30 @@ const cors = require("cors");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const port = 4000;
+const mongoose = require("mongoose");
+
+const url = "mongodb://127.0.0.1:27017/editor";
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+const mongo = mongoose.connection;
+
+mongo.once("open", () => {
+  console.log("Database connected:", url);
+});
+
+mongo.on("error", (err) => {
+  console.error("connection error:", err);
+});
+
+const draftSchema = new mongoose.Schema(
+  { value: String, editorId: String, _id: Number },
+  { _id: false }
+);
+const Draft = mongoose.model("drafts", draftSchema);
 
 app.use(
   cors({
@@ -10,11 +34,13 @@ app.use(
   })
 );
 
-const db = {};
-
 io.on("connection", function(socket) {
-  socket.on("changeEditor", (data) => {
-    db[data.editorId] = data.value;
+  socket.on("changeEditor", async (data) => {
+    await Draft.findByIdAndUpdate(
+      data.editorId,
+      { value: JSON.stringify(data.value) },
+      { upsert: true }
+    );
     io.emit(`updateEditor-${data.editorId}`, data);
   });
 });
@@ -23,8 +49,9 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 const initialValue = "A paragraph from server";
 
-app.get("/editor/:editorId/init", (req, res) => {
-  res.json(db[req.params.editorId] || initialValue);
+app.get("/editor/:editorId/init", async (req, res) => {
+  const draft = await Draft.findById(req.params.editorId);
+  res.json(draft ? JSON.parse(draft.value) : initialValue);
 });
 
 http.listen(port, () => console.log(`Example app listening on port ${port}!`));
