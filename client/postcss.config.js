bc0f34bad760a5351de8d529a5f5e1af66e3d2e1
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-03-29 10:58:46 +0800

    [CONFIG]: fix css build chain

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -4,13 +4,14 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./src/**/*.tsx", "./public/index.html"],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 });
 
 module.exports = {
   plugins: [
+    require("postcss-import"),
     tailwindcss("./tailwind.config.js"),
     require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-  ]
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
 };
