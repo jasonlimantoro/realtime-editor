6b54b757cf468cc597b5b3027e18633614d3d6d7
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-03-31 11:39:22 +0800

    [FEAT]: add groupby view

--- client/src/routes/Editor/ListEditor/ListEditor.tsx
+++ client/src/routes/Editor/ListEditor/ListEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
 import { DraftSchema } from "src/lib/entities/draft";
@@ -20,29 +20,50 @@ const ListEditor: React.FC<Props> = () => {
     setEditors((e) => e.filter(({ _id }) => _id !== id));
     service.destroy(id);
   };
+  const groupByDate = useMemo(() => {
+    const grouped = editors.reduce((accum: any, current) => {
+      const day = moment(Number(current._id)).format("YYYY-MM-DD");
+      return {
+        ...accum,
+        [day]: accum[day]?.length > 0 ? accum[day].concat(current) : [current],
+      };
+    }, {});
+    return grouped;
+  }, [editors]);
   return (
     <div>
       <h2 className="text-2xl">List Editor ({editors.length})</h2>
       <Link className="inline-block btn btn-gray" to="/editor">
         Add new editor
       </Link>
-      <ul className="mt-4">
-        {editors.map((e: DraftSchema) => (
-          <li key={e._id}>
-            <div className="flex">
-              <Link className="underline" to={`/editor/${e._id}`}>
-                {ellipsis(e.title)}
-              </Link>
-              <p className="ml-4">
-                created at {moment(Number(e._id)).format("YYYY-MM-DD hh:mm")}
-              </p>
-              <button className="ml-4" onClick={() => handleDelete(e._id)}>
-                Delete
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {Object.keys(groupByDate).map((k) => {
+        const value = groupByDate[k];
+        return (
+          <div key={k} className="mt-4">
+            <p className="text-2xl">{k}</p>
+            <ul>
+              {value.map((v: DraftSchema) => (
+                <li key={v._id}>
+                  <div className="flex">
+                    <Link className="underline" to={`/editor/${v._id}`}>
+                      {ellipsis(v.title)}
+                    </Link>
+                    <p className="ml-4">
+                      created at {moment(Number(v._id)).format("hh:mm")}
+                    </p>
+                    <button
+                      className="ml-4"
+                      onClick={() => handleDelete(v._id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
